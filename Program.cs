9638f22b5a9169d373cb0f4fe91f3597d23fc164
bhumika1111 <bhoyarpatil@434>
2022-08-04 19:42:28 +0530

    Logical Problems

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LogicalProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             Console.WriteLine("****Output For FibonacciSeries****");
+             FibonacciSeries.CalculateFibonacieSeries();
+
+             Console.WriteLine(":::::::::::::::::::::::::::::::::::::::");
+             Console.WriteLine("****Output For Perfect Number*****");
+             PerfectNumber.Checknumber();
+             
+
+             Console.WriteLine("::::::::::::::::::::::::::::::::::::::");
+             Console.WriteLine("*****Output For Prime Number*****");
+             PrimeNumber.CheckPrimeNumber();
+
+             Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+             Console.WriteLine("*****Output For Reverse Number******");
+             ReverseNumber.CheckingreverseNumber();
+
+              Console.WriteLine(":::::::::::::::::::::::::::::::::::::");
+              Console.WriteLine("*****Output For StopWatch******");
+              StopWatch.CheckingStopWatch();
+           
+            
+
+
+        }
+    }
+}
