5d3266c75b047d47ea61dcbb9dcd5222068fbf8b
bhumika1111 <bhoyarpatil@434>
2022-08-04 19:55:28 +0530

    Checking The Reverse Number

---
+++ ReverseNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProblems
+{
+    /// <summary>
+    /// Here We Check The Reverse Number ,In That We Check The Output Of Code Is A Number Obtained By Writing Original Number From Right To Left.
+    /// </summary>
+     public static   class ReverseNumber
+    {
+        public static void CheckingreverseNumber()
+        {
+
+            int n, reverse = 0, rem;
+            Console.WriteLine("** Enter the number :  **");
+            n = int.Parse(Console.ReadLine());
+
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+
+            }
+            Console.WriteLine("**Reversed Number:" + reverse + "**");
+
+        }
+    }
+}
