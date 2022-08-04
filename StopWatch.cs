8bff833b468fb61f9c1d5e8f1bec0399da2d7b17
bhumika1111 <bhoyarpatil@434>
2022-08-04 19:57:37 +0530

    Checking The Elapsed Time On StopWatch

---
+++ StopWatch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace LogicalProblems
+{
+    /// <summary>
+    /// Here We Check The Output For StopWatch In That We Check The Elapsed Time .
+    /// </summary>
+    public static  class StopWatch
+    { 
+      /// <summary>
+     /// Checking Stopwatch .
+    /// </summary>
+        public static void CheckingStopWatch()
+        {
+            Console.WriteLine("**Welcome To Stop Watch Program ! Please Enter**");
+            Console.ReadLine();
+            //First I'm Creating Object Here For Class Stopwatch.
+            Stopwatch stopwatch = new Stopwatch();
+            
+            //I Want To Start The Watch Thats Why I'm Using  Start Method Here.
+            stopwatch.Start();
+
+
+            Console.WriteLine("**Wait! Until You Want To Check The Elapsed Time On StopWatch");
+            Console.ReadLine();
+
+            //I want To Stop The Watch Thats Why I'm Using Stop Method Here.
+            stopwatch.Stop();
+
+            //I Want To Check TimeSpan Here,Timespan Is Class And Giving The Name As ts Here. 
+            TimeSpan ts = stopwatch.Elapsed;
+            Console.WriteLine("Wow! You Got The Elapsed Time");
+            Console.WriteLine(ts);
+
+        }
+    }
+}
